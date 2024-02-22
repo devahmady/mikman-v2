765a94034ca4e38f5bbbb7b45b0eb90bf55ac920
devahmady <126337569+devahmady@users.noreply.github.com>
2024-02-22 15:32:46 -0500

    Update SettingController.php

--- app/Http/Controllers/Mikrotik/SettingController.php
+++ app/Http/Controllers/Mikrotik/SettingController.php
@@ -18,13 +18,8 @@ class SettingController extends Controller
         ]);
 
         $dns = $request->input('dns');
-
-        // Jalankan perintah nslookup untuk mendapatkan informasi DNS
         $nslookup_result = shell_exec("nslookup $dns");
-
-        // Jalankan perintah ping untuk mendapatkan hasil ping
         $ping_result = shell_exec("ping -c 4 $dns");
-
         return view('admin/setting/ping')->with([
             'ping_result' => $ping_result,
             'nslookup_result' => $nslookup_result,
